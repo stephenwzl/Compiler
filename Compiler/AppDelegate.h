36290ec6cfbd19a25de2662ca9c1e8e837ab2c08
王志龙 <991405736qq@gmail.com>
2015-06-23 08:15:24 +0800

    Initial Commit

---
+++ Compiler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Compiler
+//
+//  Created by 王志龙 on 15/6/23.
+//  Copyright (c) 2015年 wangzhilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Compiler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Compiler
+//
+//  Created by 王志龙 on 15/6/23.
+//  Copyright (c) 2015年 wangzhilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
